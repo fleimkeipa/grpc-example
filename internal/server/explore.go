26b8f9e1f8d0b659e1ec321dc0fd40a6d0d8dc27
fleimkeipa <adem_sahin12@hotmail.com>
2025-10-17 17:06:03 +0300

    Update error handling on explore server

--- internal/server/explore.go
+++ internal/server/explore.go
@@ -2,12 +2,14 @@ package server
 
 import (
 	"context"
-	"errors"
 	"unicode"
 
 	"github.com/fleimkeipa/grpc-example/internal/models"
 	"github.com/fleimkeipa/grpc-example/internal/repository"
 	pb "github.com/fleimkeipa/grpc-example/proto"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type ExploreServer struct {
@@ -21,15 +23,15 @@ func NewExploreServer(repo *repository.DecisionRepository) *ExploreServer {
 
 func (s *ExploreServer) PutDecision(ctx context.Context, req *pb.PutDecisionRequest) (*pb.PutDecisionResponse, error) {
 	if req.ActorUserId == req.RecipientUserId {
-		return nil, errors.New("you can't like yourself (at least this project)")
+		return nil, status.Error(codes.InvalidArgument, "you can't like yourself (at least this project)")
 	}
 
 	if !isNumeric(req.ActorUserId) || len(req.ActorUserId) == 0 {
-		return nil, errors.New("actor id must be number")
+		return nil, status.Error(codes.InvalidArgument, "actor id must be number")
 	}
 
 	if !isNumeric(req.RecipientUserId) || len(req.RecipientUserId) == 0 {
-		return nil, errors.New("recipient id must be number")
+		return nil, status.Error(codes.InvalidArgument, "recipient id must be number")
 	}
 
 	decision := &models.Decision{
@@ -39,12 +41,12 @@ func (s *ExploreServer) PutDecision(ctx context.Context, req *pb.PutDecisionRequ
 	}
 
 	if err := s.repo.PutDecision(ctx, decision); err != nil {
-		return nil, err
+		return nil, status.Error(codes.Internal, err.Error())
 	}
 
 	mutual, err := s.repo.IsMutual(ctx, req.ActorUserId, req.RecipientUserId)
 	if err != nil {
-		return nil, err
+		return nil, status.Error(codes.Internal, err.Error())
 	}
 
 	return &pb.PutDecisionResponse{MutualLikes: mutual && req.LikedRecipient}, nil
@@ -53,7 +55,7 @@ func (s *ExploreServer) PutDecision(ctx context.Context, req *pb.PutDecisionRequ
 func (s *ExploreServer) CountLikedYou(ctx context.Context, req *pb.CountLikedYouRequest) (*pb.CountLikedYouResponse, error) {
 	count, err := s.repo.CountLikedYou(ctx, req.RecipientUserId)
 	if err != nil {
-		return nil, err
+		return nil, status.Error(codes.Internal, err.Error())
 	}
 
 	return &pb.CountLikedYouResponse{Count: uint64(count)}, nil
@@ -68,7 +70,7 @@ func (s *ExploreServer) ListLikedYou(ctx context.Context, req *pb.ListLikedYouRe
 
 	decisions, nextToken, err := s.repo.ListLikedYou(ctx, req.RecipientUserId, paginationToken)
 	if err != nil {
-		return nil, err
+		return nil, status.Error(codes.Internal, err.Error())
 	}
 
 	var likers []*pb.ListLikedYouResponse_Liker
@@ -96,7 +98,7 @@ func (s *ExploreServer) ListNewLikedYou(ctx context.Context, req *pb.ListLikedYo
 
 	decisions, nextToken, err := s.repo.ListNewLikedYou(ctx, req.RecipientUserId, paginationToken)
 	if err != nil {
-		return nil, err
+		return nil, status.Error(codes.Internal, err.Error())
 	}
 
 	var likers []*pb.ListLikedYouResponse_Liker
